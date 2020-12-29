114f3d869146b9be5ab859ff50140712934f2516
mragjale <ghanshyammragjale@gmail.com>
2020-12-29 16:10:53 +0530

    resolve the issue

--- src/main/java/com/whizit/irl/whizpay/pay/PayController.java
+++ src/main/java/com/whizit/irl/whizpay/pay/PayController.java
@@ -1,9 +1,11 @@
 package com.whizit.irl.whizpay.pay;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,24 +17,25 @@ public class PayController {
 	public PayController(PayService service) {
 		this.payService = service;
 	}
-	
+
 	@GetMapping("/test")
-    public String test(){
-        return "Hello World";
-    }
+	public String test() {
+		return "Hello World";
+	}
 
 	@PostMapping("/success/")
-	public String create(@RequestBody PayDto payDto){
+	public String create(@RequestBody PayDto payDto) {
 		return payService.success(payDto);
 	}
 
-	@PostMapping("/fail/")
-	public String fail(@RequestBody PayDto payDto){
+	// @PostMapping("/fail/")
+	@RequestMapping(value = "/fail/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public String fail(PayDto payDto) {
 		return payService.fail(payDto);
 	}
 
 	@PostMapping("/cancel/")
-	public String cancel(@RequestBody PayDto payDto){
+	public String cancel(@RequestBody PayDto payDto) {
 		return payService.cancel(payDto);
 	}
 }
